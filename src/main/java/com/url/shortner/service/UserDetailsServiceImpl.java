0aa37331be6545800fde3c7f8b8c6767055dc92c
Vicky Jeswani <jeswanivicky@gmail.com>
2025-07-09 19:38:35 +0530

    login functionality

--- src/main/java/com/url/shortner/service/UserDetailsServiceImpl.java
+++ src/main/java/com/url/shortner/service/UserDetailsServiceImpl.java
@@ -21,7 +21,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: "+ username));
-
+        System.out.println("Found user, password from DB: " + user.getPassword());
         return UserDetailsImpl.build(user);
     }
 }
